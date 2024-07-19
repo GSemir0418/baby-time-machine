a00855ed74dfd79db7a216ef282b0149bcf6f15d
gsemir0418 <845217811@qq.com>
2024-07-19 10:37:07 +0800

    feat: 初始化 prisma postgresql

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const db = globalThis.prisma || new PrismaClient()
+
+// 防止开发环境的热重载建立过多的数据库连接
+// globalThis 不会受到 hot reload 的影响
+if (process.env.NODE_ENV !== 'production')
+  globalThis.prisma = db
