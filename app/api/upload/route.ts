f919abeecd0c80a0c67d64f17475d7148f389e7e
gsemir0418 <845217811@qq.com>
2024-07-19 09:59:12 +0800

    feat: 使用 sharp 生成预览图

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,14 +2,16 @@ import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import { NextResponse } from 'next/server'
 import { v4 as uuidv4 } from 'uuid'
+import sharp from 'sharp'
 
 export async function POST(req: Request) {
   try {
-    // 自带解析 formData 的方法
-    const formData = await req.formData()
+    // 准备图片存储路径
+    const savePath = path.join(process.cwd(), `public/upload/`)
 
+    // 解析 formData
+    const formData = await req.formData()
     const pic = formData.get('pic')
-    console.log(pic)
 
     if (!pic || !(pic instanceof File)) {
       return NextResponse.json({ error: '请上传有效照片' }, { status: 400 })
@@ -18,21 +20,22 @@ export async function POST(req: Request) {
     const buffer = Buffer.from(await pic.arrayBuffer())
 
     const filename = `${uuidv4()}.${pic.name.split('.').pop()}`
+    const fileThumbName = `${filename}_thumbnail.jpg`
 
-    await writeFile(
-      // 本地存放路径
-      path.join(process.cwd(), `public/upload/${filename}`),
-      buffer,
-    )
+    // 存储到本地
+    await writeFile(path.join(savePath, filename), buffer)
 
-    // 生成预览图
-    
+    // 使用 sharp 生成预览图
+    await sharp(buffer)
+      .resize(200, 200)
+      .jpeg({ quality: 80 }) // 设置图片质量
+      .toFile(path.join(savePath, fileThumbName))
 
     // TODO：将图片路径\描述\EXIF存储到数据库
 
     const resource = {
       // 返回预览路径
-      url: `http://localhost:3000/upload/${filename}`,
+      thumbURL: `http://localhost:3000/upload/${fileThumbName}`,
     }
     return NextResponse.json({ resource, status: 201 })
   }
