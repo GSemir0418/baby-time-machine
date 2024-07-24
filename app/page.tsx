065d2720ae953e71b919107cb66b0f790e0d3d4e
gsemir0418 <845217811@qq.com>
2024-07-24 10:28:43 +0800

    feat: 首页数据渲染，footer 美化

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
+import { db } from "@/lib/db"
+
+export default async function Home() {
+  const images = await db.images.findMany()
 
   return (
-    <div>
-      This is Home page
+    <div className="bg-transparent grid grid-cols-3 sm:grid-cols-4 gap-1 p-2">
+      {images.map(i => (
+        <img key={i.id} className="rounded-lg" src={i.thumb_url} />
+      ))}
     </div>
   )
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -14,7 +14,6 @@ export const Footer = () => {
   const pathname = usePathname()
   const router = useRouter()
 
-  console.log(1231, pathname)
   const handleModalOpen = () => {
     onOpen('create')
   }
@@ -28,12 +27,12 @@ export const Footer = () => {
   return (
     <div
       className="
-        fixed bottom-0 w-full p-2 bg-indigo-300 text-white rounded-t-xl
+        fixed bottom-0 w-full p-2 bg-white text-zinc-500 rounded-t-xl
         flex flex-row justify-between items-center
       "
     >
       <div
-        className={cn("flex-1 text-center", {"bg-black": pathname===routesMap.home})}
+        className={cn("flex-1 text-center", {"text-lg font-bold text-pink-300": pathname===routesMap.home})}
         onClick={() => handleRouter('home')}
       >
         首页
@@ -41,16 +40,16 @@ export const Footer = () => {
       <div className="flex-1 text-center">
         <div
           className="
-            absolute bg-rose-300 -translate-x-[50%] left-[50%] bottom-[50%] rounded-full w-14 h-14 border shadow-xl
+            absolute bg-white -translate-x-[50%] left-[50%] bottom-[20%] rounded-full w-12 h-12
             flex justify-center items-center
             active:shadow-inner
           "
         >
-          <button onClick={handleModalOpen}><FaPlus size={20} /></button>
+          <button className="bg-pink-300 shadow-xl rounded-full w-[80%] h-[80%] flex justify-center items-center" onClick={handleModalOpen}><FaPlus size={20} /></button>
         </div>
       </div>
       <div
-        className={cn("flex-1 text-center", {"bg-black": pathname===routesMap.timeLine})}
+        className={cn("flex-1 text-center", {"text-lg font-bold text-pink-300": pathname===routesMap.timeLine})}
         onClick={() => handleRouter('timeLine')}
       >
         时间轴
